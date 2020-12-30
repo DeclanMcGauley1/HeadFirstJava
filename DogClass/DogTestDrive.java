bab06ed9f0c1b0c3ea3e231aa1a59b4d4cd31f9a
DeclanMcGauley <73233273+DeclanMcGauley1@users.noreply.github.com>
2020-12-30 00:33:38 +0000

    changes to dog class

--- DogClass/DogTestDrive.java
+++ DogClass/DogTestDrive.java
@@ -3,5 +3,14 @@ public class DogTestDrive {
   public static void main(String[] args) {
     Dog d = new Dog();
     d.bark();
+
+    Dog[] pets;
+    pets = new Dog[7];
+
+    for (int i = 0; i < 7; i++) {
+      pets[i] = new Dog();
+    }
+
+    pets[0].bark();
   }
 }
